ad2f6d1c1158a139e45a6870f50002710c41ac47
Candida Noronha <64965389+Candida18@users.noreply.github.com>
2021-05-14 12:04:18 +0530

    Delete Project directory

--- Project/mysite/homepage/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-class UserDetails(models.Model):
-    email = models.CharField(max_length=100)
-    password = models.CharField(max_length=50)
-
-class option(models.Model):
-    option = models.CharField(max_length=100)
